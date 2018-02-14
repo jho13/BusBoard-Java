9a5cf46848910bbb764d14572db27c2846077dd4
Jonghoon <32865126+jho13@users.noreply.github.com>
2018-02-14 17:08:45 +0000

    moved BusInfo construction to Website.busInfo()

--- src/main/java/training/busboard/web/BusInfo.java
+++ src/main/java/training/busboard/web/BusInfo.java
@@ -2,15 +2,9 @@ package training.busboard.web;
 
 import training.busboard.*;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class BusInfo {
-
-    private static final PostcodeClient postcodeClient = new PostcodeClient();
-    private static final TFLClient tflClient = new TFLClient();
 
     private final String postcode;
     private final List<StopWithArrivals> stopsWithArrivals;
@@ -23,41 +17,8 @@ public class BusInfo {
         return stopsWithArrivals;
     }
 
-    public BusInfo(String postcode) {
+    public BusInfo(String postcode, List<StopWithArrivals> stopsWithArrivals) {
         this.postcode = postcode;
-
-        Coordinates coordinates = postcodeClient.getCoordinate(postcode);
-        if (coordinates == null) {
-            this.stopsWithArrivals = null;
-            return;
-        }
-
-        this.stopsWithArrivals = new ArrayList<>();
-        List<StopPoint> nearbyStopPoints = tflClient.getNearbyStopPoints(coordinates);
-        if (nearbyStopPoints.isEmpty()) {
-            return;
-        }
-
-        Collections.sort(nearbyStopPoints, new Comparator<StopPoint>() {
-            @Override
-            public int compare(StopPoint o1, StopPoint o2) {
-                return o1.getDistance() - o2.getDistance();
-            }
-        });
-
-        for (int i = 0; i < nearbyStopPoints.size() && i < 2; ++i) {
-            StopPoint stop = nearbyStopPoints.get(i);
-            List<ArrivalPrediction> predictions =
-                    tflClient.getPredictions(stop.getNaptanId());
-            Collections.sort(predictions, new Comparator<ArrivalPrediction>() {
-                @Override
-                public int compare(ArrivalPrediction ap1, ArrivalPrediction ap2) {
-                    return ap1.getTimeToStation() - ap2.getTimeToStation();
-                }
-            });
-            predictions = predictions.size() > 5 ? predictions.subList(0, 5) : predictions;
-            stopsWithArrivals.add(new StopWithArrivals(stop.getCommonName(), predictions));
-        }
+        this.stopsWithArrivals = stopsWithArrivals;
     }
-
 }
--- src/main/java/training/busboard/web/Website.java
+++ src/main/java/training/busboard/web/Website.java
@@ -6,10 +6,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import training.busboard.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 @Controller
 @EnableAutoConfiguration
 public class Website {
+
+    private static final PostcodeClient postcodeClient = new PostcodeClient();
+    private static final TFLClient tflClient = new TFLClient();
 
     @RequestMapping("/")
     ModelAndView home() {
@@ -18,11 +27,41 @@ public class Website {
 
     @RequestMapping("/busInfo")
     ModelAndView busInfo(@RequestParam("postcode") String postcode) {
-        return new ModelAndView("info", "busInfo", new BusInfo(postcode)) ;
+        List<StopWithArrivals> stopsWithArrivals;
+
+        Coordinates coordinates = postcodeClient.getCoordinate(postcode);
+        if (coordinates == null) {
+            return new ModelAndView("info", "busInfo",
+                    new BusInfo(postcode, null)) ;
+        }
+
+        stopsWithArrivals = new ArrayList<>();
+
+        List<StopPoint> nearbyStopPoints = tflClient.getNearbyStopPoints(coordinates);
+        Collections.sort(nearbyStopPoints, new Comparator<StopPoint>() {
+            @Override
+            public int compare(StopPoint o1, StopPoint o2) {
+                return o1.getDistance() - o2.getDistance();
+            }
+        });
+
+        for (int i = 0; i < nearbyStopPoints.size() && i < 2; ++i) {
+            StopPoint stop = nearbyStopPoints.get(i);
+            List<ArrivalPrediction> predictions =
+                    tflClient.getPredictions(stop.getNaptanId());
+            Collections.sort(predictions, new Comparator<ArrivalPrediction>() {
+                @Override
+                public int compare(ArrivalPrediction ap1, ArrivalPrediction ap2) {
+                    return ap1.getTimeToStation() - ap2.getTimeToStation();
+                }
+            });
+            predictions = predictions.size() > 5 ? predictions.subList(0, 5) : predictions;
+            stopsWithArrivals.add(new StopWithArrivals(stop.getCommonName(), predictions));
+        }
+        return new ModelAndView("info", "busInfo", new BusInfo(postcode, stopsWithArrivals)) ;
     }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Website.class, args);
     }
-
 }
